80c0c30a4894570cc88a532d2aa9cc76b85d5179
komiwalnut <62009152+komiwalnut@users.noreply.github.com>
2025-02-02 23:00:28 +0800

    Update fetch_sales.py

--- src/fetch_sales.py
+++ src/fetch_sales.py
@@ -2,7 +2,7 @@ import aiohttp
 
 
 async def fetch_sales():
-    url = "https://skynet-api.roninchain.com/ronin/explorer/v2/collections/0xee9436518030616bc315665678738a4348463df4/transfers?offset=0&limit=15"
+    url = "https://skynet-api.roninchain.com/ronin/explorer/v2/collections/0xee9436518030616bc315665678738a4348463df4/transfers?offset=0&limit=50"
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             if response.status == 200:
