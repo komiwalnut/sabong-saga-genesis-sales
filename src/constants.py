c51da125028aaeb422df9da197e0339d1a6fd92b
komiwalnut <62009152+komiwalnut@users.noreply.github.com>
2025-02-06 00:34:26 +0800

    Create constants.py

---
+++ src/constants.py
@@ -0,0 +1,6 @@
+TOKEN_MAPPING = {
+    "0xc99a6a985ed2cac1ef41640596c5a5f9f4e19ef5": ("WETH", 1e18),
+    "0x97a9107c1793bc407d6f527b77e7fff4d812bece": ("AXS", 1e18),
+    "0x0b7007c13325c48911f73a2dad5fa5dcbf808adc": ("USDC", 1e6),
+    "0x1a89ecd466a23e98f07111b0510a2d6c1cd5e400": ("BANANA", 1e18)
+}
